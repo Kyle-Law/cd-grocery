cfdcde4d6fc15b68a91b6479df9e36c2ad5a36ea
kyle-law <junwei_1121@hotmail.com>
2020-07-18 16:03:27 +0800

    Added NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const NavBar = () => (
+  <>
+    <nav>
+      <ul>
+        <li>
+          <a>Mart Name</a>
+        </li>
+        <li>
+          <input type="text" placeholder="Search in Mart"></input>
+        </li>
+        <li>
+          <p>My Lists</p>
+        </li>
+        <li>
+          <p>Sign Up</p>
+        </li>
+      </ul>
+    </nav>
+  </>
+);
+
+export default NavBar;
