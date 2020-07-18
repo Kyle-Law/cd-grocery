46e211832df5e9584f873462947c08ba5d81d32d
kyle-law <junwei_1121@hotmail.com>
2020-07-18 23:24:38 +0800

    Updates

---
+++ precache-manifest.bfc2db81cd307c598a65e45b87509a7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724856792825198cb37f3ff9ae4ce9cd",
+    "url": "/cd-grocery/index.html"
+  },
+  {
+    "revision": "417bea8003824341147e",
+    "url": "/cd-grocery/static/css/main.1a7de266.chunk.css"
+  },
+  {
+    "revision": "f10b867679ebeccbbbab",
+    "url": "/cd-grocery/static/js/2.bbf7f9cf.chunk.js"
+  },
+  {
+    "revision": "8bc88370bc2489d86f2f357f447ddb0b",
+    "url": "/cd-grocery/static/js/2.bbf7f9cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "417bea8003824341147e",
+    "url": "/cd-grocery/static/js/main.1c8f753b.chunk.js"
+  },
+  {
+    "revision": "4d34368378d8fedc69f9",
+    "url": "/cd-grocery/static/js/runtime-main.8703a385.js"
+  }
+]);
